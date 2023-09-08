bdcd046ac4e09ed296ea696f2621f95a10e89dea
Caroline Avery Tanto <75901018+carolinetanto@users.noreply.github.com>
2023-09-08 13:43:13 +0800

    done

---
+++ try.js
@@ -0,0 +1,33 @@
+function getCountryData() {
+    const countryInput = document.getElementById('countryInput').value;
+  
+    fetch(`https://covid-193.p.rapidapi.com/statistics?country=${countryInput}`, {
+        method: 'GET',
+        headers: {
+        'x-rapidapi-host': 'covid-193.p.rapidapi.com',
+        'x-rapidapi-key': '64e5eec2ddmsh93ce568d77b4254p12f91bjsn3647df4b42ad'
+        }
+    })
+    .then(response => response.json())
+    .then(data => {
+        const countryData = data.response[0];
+
+        const activeCases = countryData.cases.active;
+        const newCases = countryData.cases.new;
+        const recoveredCases = countryData.cases.recovered;
+        const totalCases = countryData.cases.total;
+        const totalDeaths = countryData.deaths.total;
+        const totalTests = countryData.tests.total;
+
+        document.getElementById('activeCases').textContent = activeCases;
+        document.getElementById('newCases').textContent = newCases;
+        document.getElementById('recoveredCases').textContent = recoveredCases;
+        document.getElementById('totalCases').textContent = totalCases;
+        document.getElementById('totalDeaths').textContent = totalDeaths;
+        document.getElementById('totalTests').textContent = totalTests;
+    })
+    .catch(error => {
+        console.log(error);
+        alert('An error occurred while fetching data.');
+    });
+}
